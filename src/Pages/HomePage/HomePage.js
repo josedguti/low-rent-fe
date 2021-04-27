00f51106b0d81a9e0d4058d596dcb0cada676649
Tyler Ray <tyler.p.ray@icloud.com>
2021-04-27 16:23:03 -0400

    Added footer to app.js

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import Header from '../../components/Header/Header';
 function HomePage(props) {
     return (
         <div>
-        <Header />
+
         <p>This is the Home Page</p>
         
         </div>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+function Footer(props) {
+    return (
+        <div className="footer"><strong>Copyright &copy; All Rights Reserved 2021 Clothing Site</strong></div>
+    );
+}
+
+
+export default Footer;

3ab613d3a552994f0f9d094c0a55d276c5894003
Jessica Paul <paul.jessica95@gmail.com>
2021-04-27 14:29:13 -0400

    create Nav component and basic CSS layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
        Home Page
        <Header />

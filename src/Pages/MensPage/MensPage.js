8827892480ae3481859c895e9d13fd14e36aead0
Tyler Ray <tyler.p.ray@icloud.com>
2021-04-27 17:25:16 -0400

    added clothing card component

--- src/Pages/MensPage/MensPage.js
+++ src/Pages/MensPage/MensPage.js
@@ -1,7 +1,10 @@
+import ClothingCard from '../../components/ClothingCard/ClothingCard';
+
 function MensPage(props) {
     return (
         <div>
         <p>This is the Mens Page</p>
+        <ClothingCard/>
         
         </div>
     );

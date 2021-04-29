03e1af78118b223bd4a361c5c47d16baa9234596
Jessica Paul <paul.jessica95@gmail.com>
2021-04-29 14:59:47 -0400

    add AJAX functions for wishlist model

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,9 +1,43 @@
-const BASE_URL = 'http://localhost:3000/clothes';
+const BASE_URL = 'http://localhost:3000';
 
 function fetchClothes() {
-    return fetch(BASE_URL).then(res => res.json());
+    return fetch(`${BASE_URL}/clothes`).then(res => res.json());
+}
+
+function fetchWishlists() {
+    return fetch(`${BASE_URL}/wishlists`).then(res => res.json());
+}
+
+function createWishlist(data) {
+    return fetch(`${BASE_URL}/wishlists`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json());
+}
+
+function deleteWishlist(id) {
+    return fetch(`${BASE_URL}/${id}`, {
+        method: 'DELETE'
+    }).then(res => res.json());
+}
+
+function updateWishlist({ userId, id }) {
+    return fetch(`${BASE_URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ userId })
+    }).then(res => res.json());
 }
 
 export {
     fetchClothes,
+    fetchWishlists,
+    createWishlist,
+    deleteWishlist,
+    updateWishlist
 }

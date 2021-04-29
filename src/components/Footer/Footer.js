5d5281f2cfda85cb7a260451f70e0df1c7c9a798
Jose Gutierrez <josegutierrez@Joses-Mac-mini.fios-router.home>
2021-04-29 13:15:29 -0400

    added social media icons to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,18 @@
 function Footer(props) {
     return (
-        <div className="footer">Copyright &copy; All Rights Reserved 2021 Low-Rent</div>
+        <>
+        <div className="footer">
+            Copyright &copy; All Rights Reserved 2021 Low-Rent
+            <br></br><br></br>
+
+            <a href='#'><img src='https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Instagram-512.png' alt='instagram icon'/></a>
+            <a href='#'><img src='https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_square-512.png' alt='facebook icon'/></a>
+            <a href='#'><img src='https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter3_colored_svg-512.png' alt='twitter icon'/></a>
+
+            
+        </div>
+        
+        </>
     );
 }
 

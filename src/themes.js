88a34e6f63a7f1419f30f2c446af81cc64707639
tyler-ray90 <tyler_ray98@yahoo.com>
2021-04-30 12:13:01 -0400

    Darkmode working with global stling

--- src/themes.js
+++ src/themes.js
@@ -3,11 +3,13 @@ import {createGlobalStyle} from 'styled-components';
 export const lightTheme = {
     body: "#fff",
     fontColor: "#000",
+    linkcolor: '#000'
 };
 
 export const darkTheme = {
     body: "#000",
     fontColor: "#fff",
+    linkcolor: '#fff',
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -15,5 +17,10 @@ export const GlobalStyles = createGlobalStyle`
     body {
         background-color: ${props => props.theme.body};
     }
+    a {
+        color: ${props => props.theme.linkcolor};
+        text-decoration: none;
+    }
+
 
 `;

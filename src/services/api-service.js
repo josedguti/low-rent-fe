311b1c166dd2ab1b88af2f125cb79934c024ed4a
Jose Gutierrez <josegutierrez@Joses-Mac-mini.fios-router.home>
2021-05-02 15:55:13 -0400

    backend URL updated

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://low-rent-backend.herokuapp.com';
 
 function fetchClothes() {
     return fetch(`${BASE_URL}/clothes`).then(res => res.json());
